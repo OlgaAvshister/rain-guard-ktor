f5a5adf45317cf0030e797cb77d53d61f3c66a80
OlgaAvshister <olya.avshister@yandex.ru>
2026-04-03 00:44:23 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "rain-guard-ktor"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package com.olga.avshister
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.cio.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureRouting()
+}
