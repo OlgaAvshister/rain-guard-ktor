c34638464a0b4946a6371e98b731d28ee13b9361
OlgaAvshister <olya.avshister@yandex.ru>
2026-04-30 22:44:59 +0500

    Give empty rent point list instead of 404

--- src/main/kotlin/features/rentPoint/RentPointController.kt
+++ src/main/kotlin/features/rentPoint/RentPointController.kt
@@ -1,11 +1,8 @@
 package com.olga.avshister.features.rentPoint
 
-import com.olga.avshister.domain.Product
-import com.olga.avshister.domain.Product.Companion.toDTO
 import com.olga.avshister.domain.Rent
 import com.olga.avshister.domain.RentPoint.Companion.toDTO
 import com.olga.avshister.services.RentPointService
-import com.olga.avshister.services.RentService
 import com.olga.avshister.services.UsersService
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.ApplicationCall
@@ -15,14 +12,10 @@ class RentPointController(private val call: ApplicationCall) {
     suspend fun getRentPoints(token: String) {
         try {
             val rentPoints = RentPointService.getRentPoints(token)
-            if (rentPoints.isEmpty()) {
-                call.respond(HttpStatusCode.NotFound, "Ошибка загрузки точек аренды/возврата")
-            } else {
-                call.respond(
-                    HttpStatusCode.OK,
-                    rentPoints.map { it.toDTO() }
-                )
-            }
+            call.respond(
+                HttpStatusCode.OK,
+                rentPoints.map { it.toDTO() }
+            )
         } catch (e: Exception) {
             call.respond(HttpStatusCode.BadRequest, message = "message: ${e.message}, cause: ${e.cause}")
         }
