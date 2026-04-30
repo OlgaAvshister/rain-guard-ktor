0590ddc9c0e578a949adaf6ba0a03974392363f8
OlgaAvshister <olya.avshister@yandex.ru>
2026-04-30 20:27:23 +0500

    Implemented docker

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     kotlin("jvm") version "2.3.0"
     id("io.ktor.plugin") version "3.4.2"
     kotlin("plugin.serialization") version "2.3.0"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 group = "com.olga.avshister"
